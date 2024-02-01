e27e2a385c23ece0470ddbdf98a943b2bd50853b
sid <deshmukhsiddhant420@gmail.com>
2024-02-02 00:04:19 +0530

    add listing files

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+"""
+TODO:represent tree and help in cli
+TODO:use ccourse outcome topics (cover almost everything) 
+"""
 from pydrive2.drive import GoogleDrive
 from pydrive2.auth import GoogleAuth
 
@@ -20,13 +24,43 @@ drive = GoogleDrive(gauth)
 
 
 
-def list_files():
-    file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-    for file1 in file_list:
-        print('title: %s, id: %s' % (file1['title'], file1['id']))
-    print("Listeddd")
+# def list_files():
+#     file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+#     for file1 in file_list:
+#         print('title: %s, id: %s' % (file1['title'], file1['id']))
+#     print("Listeddd")
+#     for file_list in drive.ListFile({'q': 'trashed=true', 'maxResults': 10}):
+#         print('Received %s files from Files.list()' % len(file_list))  # <= 10
+#     for file1 in file_list:
+#         print('title: %s, id: %s' % (file1['title'], file1['id']))
 
-list_files()
+
+def get_filesystem_tree(drive, folder_id='root', depth=0):
+    
+    files = drive.ListFile({'q': f"'{folder_id}' in parents"}).GetList()
+
+    tree = []
+    for file1 in files:
+        node = {
+            'title': file1['title'],
+            'id': file1['id'],
+            'mimeType': file1['mimeType'],
+            'children': []
+        }
+
+        if file1['mimeType'] == 'application/vnd.google-apps.folder':
+            node['children'] = get_filesystem_tree(drive, file1['id'], depth + 1)
+
+        tree.append(node)
+
+    return tree
+
+
+root_folder_tree = get_filesystem_tree(drive)
+print(root_folder_tree)
+
+
+
 
 def file_upload():
 
